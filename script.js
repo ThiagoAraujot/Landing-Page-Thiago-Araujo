354fccfa324be069601e4b6f43af1829996fcc58
thiago araujo <devthiagoaraujo@gmail.com>
2024-07-11 20:22:16 -0300

    fix: scroll animations bugs

--- script.js
+++ script.js
@@ -178,3 +178,16 @@ let swiperTestimonial = new Swiper(".testimonial-container", {
     },
   },
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
